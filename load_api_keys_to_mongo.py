88edfd96bb32562c791165d7b76e3ade1ddebf1a
PRITAM KUMAR DAS <141274451+kumarpritam143@users.noreply.github.com>
2026-01-14 05:23:59 +0530

    Add files via upload

---
+++ load_api_keys_to_mongo.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+MONGO_URI = "mongodb+srv://kumardaspritam972_db_user:pk4suXsyOMGYxfiS@ai-voice-system.xm0qrh1.mongodb.net/?appName=ai-voice-system"
+
+client = MongoClient(MONGO_URI)
+db = client["ai_voice_db"]
+api_keys_col = db["api_keys"]
+
+# OPTIONAL: purane keys hata do (first time clean insert)
+api_keys_col.delete_many({})
+
+api_keys = [
+    "sk_eb5431472cbdd8b94851db4180cf877e74592c37fd71064d",
+    "sk_863ff38c6d50dc136bfb6e32a35da2c502fff64df6e29bcd"
+]
+
+docs = []
+
+for key in api_keys:
+    docs.append({
+        "provider": "elevenlabs",
+        "api_key": key,
+        "active": True,
+        "used_count": 0,
+        "last_used": None,
+        "created_at": datetime.utcnow()
+    })
+
+api_keys_col.insert_many(docs)
+
+print("✅ API keys successfully saved to MongoDB")
---
+++ load_voices_to_mongo.py
@@ -0,0 +1,22 @@
+import json
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://kumardaspritam972_db_user:pk4suXsyOMGYxfiS@ai-voice-system.xm0qrh1.mongodb.net/?appName=ai-voice-system"
+
+JSON_PATH = r"C:\Users\91628\Dropbox\PC\Downloads\Realtime-Streaming-website-main\Realtime-Streaming-website-main\elevenlabs_voices.json"
+
+client = MongoClient(MONGO_URI)
+db = client["ai_voice_db"]
+voices_col = db["voices"]
+
+# Load JSON file
+with open(JSON_PATH, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+# 🔥 Clear old data (optional but recommended)
+voices_col.delete_many({})
+
+# 🔥 DIRECT INSERT (AS-IS)
+voices_col.insert_one(data)
+
+print("✅ JSON file saved directly into MongoDB (no modification)")
